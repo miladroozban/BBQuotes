0c60956b57881b3fa80721f71acbbb292f785845
Milad Roozban <Roozbanmilad@gmail.com>
2026-05-07 13:47:44 +0330

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Milad Roozban on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
