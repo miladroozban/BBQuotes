c52d6b7b60fd6234baaa30475eab1dc4d9aeb717
Milad Roozban <Roozbanmilad@gmail.com>
2026-05-25 10:12:40 +0330

    defined models and ViewModel for manipulating data from api

---
+++ BBQuotes/Quote.swift
@@ -0,0 +1,11 @@
+//
+//  Quote.swift
+//  BBQuotes
+//
+//  Created by Milad Roozban on 5/16/26.
+//
+
+struct Quote: Decodable {
+    let quote: String
+    let character: String
+}
---
+++ BBQuotes/QuoteView.swift
@@ -0,0 +1,18 @@
+//
+//  QuoteView.swift
+//  BBQuotes
+//
+//  Created by Milad Roozban on 5/25/26.
+//
+
+import SwiftUI
+
+struct QuoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuoteView()
+}
