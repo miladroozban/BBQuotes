e2fce3cba44147767d5762ad732616a3e6cc1485
Milad Roozban <Roozbanmilad@gmail.com>
2026-06-08 22:53:06 +0330

    added characterView for showing some info about show characters

---
+++ BBQuotes/CharacterView.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterView.swift
+//  BBQuotes
+//
+//  Created by Milad Roozban on 6/3/26.
+//
+
+import SwiftUI
+
+struct CharacterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterView()
+}
