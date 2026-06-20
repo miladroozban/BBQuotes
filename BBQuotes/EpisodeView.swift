22c5e1e331fe4289fc7b40f1394b4dfb5726119b
Milad Roozban <Roozbanmilad@gmail.com>
2026-06-20 23:27:34 +0330

    completed EpisodeView, now we can see a random episode data through the main view based on eaach episode.

---
+++ BBQuotes/EpisodeView.swift
@@ -0,0 +1,18 @@
+//
+//  EpisodeView.swift
+//  BBQuotes
+//
+//  Created by Milad Roozban on 6/20/26.
+//
+
+import SwiftUI
+
+struct EpisodeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EpisodeView()
+}
